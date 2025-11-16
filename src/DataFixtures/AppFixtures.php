16da0f8a3c2ff5142aa63be9872b2f059cac92e1
Ania <annaradlinska99@gmail.com>
2025-11-16 20:16:54 +0100

    zajecia16.11

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $article = new Article();
+        $article -> setContent(
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est expedita adipisci, voluptatibus nulla reiciendis, totam sunt nam blanditiis consectetur corporis enim fugit eligendi ex eos accusamus veniam esse quae perspiciatis?'
+        );
+        $article -> setTitle('To jest artykuł #1');
+        $manager -> persist($article);
+
+        $article2 = new Article();
+        $article2 -> setContent(
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est expedita adipisci, voluptatibus nulla reiciendis, totam sunt nam blanditiis consectetur corporis enim fugit eligendi ex eos accusamus veniam esse quae perspiciatis?'
+        );
+        $article2 -> setTitle('To jest artykuł #1');
+        $manager -> persist($article2);
+
+        $manager->flush();
+    }
+}
